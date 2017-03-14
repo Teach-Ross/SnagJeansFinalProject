4224f899d0b6c92d85324ca6b603584a2557bbc9
rteach <ross.c.teach@gmail.com>
2017-03-14 16:29:30 -0400

    added userDao

---
+++ src/main/java/com/test/DAO/UserDao.java
@@ -0,0 +1,45 @@
+package com.test.DAO;
+
+import com.test.model.UserEntity;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+public class UserDao {
+    private SessionFactory sessionFactory;
+    private Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+
+    public SessionFactory getSessionFactory() {
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+        return cfg.buildSessionFactory();
+    }
+
+
+    public void insert(UserEntity user) {
+        Session session = getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void delete(UserEntity user) {
+        Session session = getSessionFactory().openSession();
+        session.beginTransaction();
+
+    }
+
+    public boolean emailExists(String email) {
+        Session session = getSessionFactory().openSession();
+        session.beginTransaction();
+        Criteria emailSearch = session.createCriteria(UserEntity.class);
+        emailSearch.add(Restrictions.eq("email", email).ignoreCase());
+        emailSearch.setProjection(Projections.rowCount());
+        Long rowCount = (Long) emailSearch.uniqueResult();
+        return (rowCount > 0);
+    }
+}
+
