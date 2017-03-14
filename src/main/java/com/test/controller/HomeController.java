c17f13f6e08275262872ca7a8f93fdef08583779
Michael P. Johnson <mpjohnson_michigan@hotmail.com>
2017-03-14 15:13:21 -0400

    added FB Login

--- src/main/java/com/test/controller/HomeController.java
+++ src/main/java/com/test/controller/HomeController.java
@@ -16,7 +16,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
 import java.util.Random;
 
 
@@ -25,6 +27,44 @@ import java.util.Random;
 public class HomeController {
 
     @RequestMapping("/")
+
+
+    public ModelAndView landingPage()
+    {
+        FBConnection fbConnection = new FBConnection();
+        return new
+                ModelAndView("landingpage","message", fbConnection.getFBAuthUrl());
+
+    }
+
+    @RequestMapping("welcome2")
+
+    public ModelAndView helloWorld2(@RequestParam("code") String code) {
+
+
+        if (code == null || code.equals("")) {
+            throw new RuntimeException (
+                    "ERROR: Didn't get code paramter in callback");
+        }
+
+        FBConnection fbConnection = new FBConnection();
+        String accessToken = fbConnection.getAccessToken(code);
+
+        FBGraph fbGraph = new FBGraph(accessToken);
+        String graph = fbGraph.getFBGraph();
+        Map fbProfileData = fbGraph.getGraphData(graph);
+
+        String out = "";
+        out = out.concat("<h1>Facebook Login using Java</h1>");
+        out = out.concat("<h2>Application Main Menu</h2>");
+        out = out.concat("<div>Welcome "+fbProfileData.get("name"));
+        out = out.concat("<div>Your Email: "+fbProfileData.get("email"));
+        out = out.concat("<div>You are "+fbProfileData.get("gender"));;
+
+        return new
+                ModelAndView("welcome2","message",out);
+
+    }
 
     public String jeansImages(Model model)
     {
---
+++ src/main/webapp/WEB-INF/views/landingpage.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mpjoh
+  Date: 3/14/2017
+  Time: 3:07 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Snag Jeans Landing Page</h1>
+<div id="left">
+    <a href="${message}">FB Login!</a>
+</div>
+
+<!--div id="right"
+<iframe width="1200" height="615"
+        src="http://www.glamour.com/story/flattering-jeans-reese-witherspoon-kendall-jenner-denim"
+        frameborder="0" allowfullscreen></iframe>
+</div -->
+</body>
+</html>
